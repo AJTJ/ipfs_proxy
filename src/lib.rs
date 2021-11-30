ef8f899697686e277e6a3855acb63052a26a2423
AJTJ <janke.aaron@gmail.com>
2021-11-30 14:08:58 -0700

    deleted test material

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,6 @@
 #[macro_use]
 extern crate diesel;
-use diesel::pg::PgConnection;
-use diesel::prelude::*;
-use dotenv::dotenv;
-use std::env;
 pub mod actions;
 pub mod endpoints;
 pub mod models;
 pub mod schema;
-
-// for testing purposes only
-pub fn establish_connection() -> PgConnection {
-    dotenv().ok();
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
-}
